391fa3fcc2a3710212a91cac0b2d3bceabe5edde
Amit Cohen <cohennamitt@gmail.com>
2023-02-22 16:58:41 +0200

    added search location function

--- js/app.controller.js
+++ js/app.controller.js
@@ -6,6 +6,12 @@ window.onAddMarker = onAddMarker
 window.onPanTo = onPanTo
 window.onGetLocs = onGetLocs
 window.onGetUserPos = onGetUserPos
+window.onSearch = onSearch
+
+function onSearch(ev) {
+    ev.preventDefault()
+    console.log('Hi');
+}
 
 function onInit() {
     mapService.initMap()
@@ -47,7 +53,7 @@ function onGetUserPos() {
             console.log('err!!!', err)
         })
 }
-function onPanTo() {
+function onPanTo(lat = 35.6895, lng = 139.6917) {
     console.log('Panning the Map')
-    mapService.panTo(35.6895, 139.6917)
+    mapService.panTo(lat, lng)
 }
